c579a6c93c07d4f4004ddbd0bb54043013986b9b
Paulo011 <pauloortega011@gmail.com>
2022-08-09 23:37:15 -0300

    criado objetos lancamento e totalizadorDoMes | ciada função distribuirDespesas

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 console.log('programa para monitorar as finanças pessoais')
 
+class Lancamento {
+    constructor(categoria, tipo, valor){
+        if(tipo !== "receita" && tipo !== "despesa"){
+            throw new Error ("Lançamento Inválido: Tipo deve ser receita ou despesa")
+        }
+
+        if(valor <= 0 ){
+            throw new Error ("lançamento Inválido: Valor deve ser maior que zero")
+        }
+
+        if(categoria === ""){
+            throw new Error ("Lançamento Inválido: A categoria é obrigatória")
+        }
+
+        this.categoria = categoria
+        this.tipo = tipo
+        this.valor = valor
+    }
+}
+
 const arredondar = (valor) => {
     return Math.round((valor * 100)/ 100)
 }
@@ -12,6 +32,18 @@ const calcJuros = (valor) => {
 const calcRendimento = (valor) => {
     let resultado = arredondar(valor) * 0.005
     return resultado
+}
+
+const distribuirDespesas = (lancamentos, total) => {
+
+    let distribuicaoDeDespesas = []
+    for (lancamentos of lancamentos){
+        if (lancamentos.tipo === 'despesa'){
+            let percentual = arredondar((lancamentos.valor/total.despesas)*100)
+            distribuicaoDeDespesas.push({categoria: lancamentos.categoria, percentual: percentual})
+        }
+    }
+    return distribuicaoDeDespesas
 }
 
 const encontraValores = (lancamentos, categoriaDaDespesa) => {
@@ -45,43 +77,50 @@ const calcPorcentagem = (lancamentos, categoriaDaDespesa) => {
     return resultado
 }
 
-const calcularSaldo = (lancamentos) => {
-    let saldo = 0
+const calcularSaldo = (saldoInicial, lancamentos) => {
+    let totalizadorDoMes = {saldo: 0,saldoInicial, juros: 0, rendimentos: 0, receita: 0, despesas: 0, distribuicaoDeDespesas: []}
+    totalizadorDoMes.saldo = saldoInicial
+
     lancamentos.forEach(lancamentos => {
         if(lancamentos.tipo == 'receita'){
-            saldo += lancamentos.valor
+            totalizadorDoMes.saldo += lancamentos.valor
+            totalizadorDoMes.receita += lancamentos.valor
         }else if (lancamentos.tipo == 'despesa'){
-            saldo -= lancamentos.valor
+            totalizadorDoMes.saldo -= lancamentos.valor
+            totalizadorDoMes.despesas += lancamentos.valor
         }else{
             console.log("erro ao ler um dos lançamentos")
         }
     })
 
-    const estaNegativo = saldo < 0
+    totalizadorDoMes.distribuicaoDeDespesas = distribuirDespesas(lancamentos, totalizadorDoMes)
+    const estaNegativo = totalizadorDoMes.saldo < 0
 
     if(estaNegativo){
-        const juros = calcJuros(saldo)
-        saldo = saldo + juros
+        totalizadorDoMes.juros = calcJuros(totalizadorDoMes.saldo)
+        totalizadorDoMes.saldo += totalizadorDoMes.juros
     }else {
-        const rendimentos = calcRendimento(saldo)
-        saldo = saldo + rendimentos
+        totalizadorDoMes.rendimentos= calcRendimento(totalizadorDoMes.saldo)
+        totalizadorDoMes.saldo += totalizadorDoMes.rendimentos
     }
-    return saldo
+    return totalizadorDoMes
 }
 
 console.log('janeiro')
 
-const lancamentosJaneiro  =[{categoria: 'salário',tipo: 'receita', valor: 3000},
-{categoria: 'aluguel',tipo: 'despesa' ,valor: 1000}, 
-{categoria: 'conta de luz',tipo: 'despesa' ,valor: 200},
-{categoria: 'conta de água',tipo: 'despesa' ,valor: 100},
-{categoria: 'internet',tipo: 'despesa' ,valor: 100},
-{categoria: 'transporte',tipo: 'despesa' ,valor: 300},
-{categoria: 'lazer',tipo: 'despesa' ,valor: 300},
-{categoria: 'alimentação',tipo: 'despesa' ,valor: 500}]
+const lancamentosJaneiro  =[ new Lancamento('salário','receita', 3000),
+new Lancamento('aluguel','despesa' ,1000), 
+new Lancamento('conta de luz', 'despesa' ,200),
+new Lancamento('conta de água','despesa' ,100),
+new Lancamento('internet','despesa' ,100),
+new Lancamento('transporte','despesa' ,300),
+new Lancamento('lazer','despesa' ,300),
+new Lancamento('alimentação','despesa' ,500)
+]
 
-const saldoJaneiro = calcularSaldo (lancamentosJaneiro)
+const saldoJaneiro = calcularSaldo (0, lancamentosJaneiro)
 
-console.log(`entre entradas e saidas do mes de janeiro o saldo é de ${saldoJaneiro}`)
+console.log(`entre entradas e saidas do mes de janeiro o saldo é de ${saldoJaneiro.saldo}`)
+console.log(saldoJaneiro)
 
 console.log(`o alugel corespode a ${calcPorcentagem(lancamentosJaneiro, "aluguel")}% do valor do salário do usúario`)
